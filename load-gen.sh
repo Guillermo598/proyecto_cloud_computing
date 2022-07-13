c1727e1c18cf2cded0591fa30e9c1648b9e116c8
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2022-07-12 21:19:18 -0500

    Create load-gen.sh

---
+++ load-gen.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+NUM_CLIENTS=5
+
+RUN_TIME=5m
+
+HOST="http://35.185.24.49:8080/"
+
+ERROR=0
+
+DAEMON="-it"
+SILENT=0
+
+USAGE="\
+loadgen.sh
+e - error flag
+d - run in background
+n - number of clients
+t - time to run n clients
+h - target host
+"
+
+# get the tag info
+eval $(egrep '[A-Z]+=' ../.env)
+
+echo "Repo $REPO"
+echo "Tag $TAG"
+
+while getopts 'edn:t:h:' OPT
+do
+    case $OPT in
+        e)
+            ERROR=1
+            ;;
+        d)
+            DAEMON="-d"
+            SILENT=1
+            ;;
+        n)
+            NUM_CLIENTS=$OPTARG
+            if echo "$NUM_CLIENTS" | egrep -q '^[0-9]+$'
+            then
+                CLIENTS=${NUM_CLIENTS:-1}
+                echo "Running $CLIENTS clients"
+            else
+                echo "$NUM_CLIENTS is not a number falling back to 1"
+                CLIENTS=1
+            fi
+            ;;
+        t)
+            RUN_TIME=$OPTARG
+            if echo "$RUN_TIME" | egrep -q '^([0-9]+h)?([0-9]+m)?$'
+            then
+                echo "Run time set to $RUN_TIME"
+            else
+                echo "Time format 1h30m"
+                echo "$USAGE"
+                exit 1
+            fi
+            ;;
+        h)
+            HOST=$OPTARG
+            if echo "$HOST" | egrep '^http://[a-z0-9]+'
+            then
+                echo "Host $HOST"
+            else
+                echo "Host must start http://"
+                echo "$USAGE"
+                exit 1
+            fi
+            ;;
+        *)
+            echo "$USAGE"
+            exit 1
+            ;;
+    esac
+done
+
+docker run \
+    $DAEMON \
+    --name loadgen \
+    --rm \
+    --network=host \
+    -e "HOST=$HOST" \
+    -e "NUM_CLIENTS=$NUM_CLIENTS" \
+    -e "RUN_TIME=$RUN_TIME" \
+    -e "SILENT=$SILENT" \
+    -e "ERROR=$ERROR" \
+    ${REPO}/rs-load:${TAG}
